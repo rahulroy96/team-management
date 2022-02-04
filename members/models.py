4398b0f1e03187d8bbf917db4b6e3ff94d9d5c3b
Rahul Roy <rahulroy1996@gmail.com>
2022-02-04 18:37:34 -0500

    member model and migration created

--- members/models.py
+++ members/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Member(models.Model):
+
+	f_name = models.CharField(max_length=100)
+	l_name = models.CharField(max_length=100)
+	email = models.EmailField()
+
+	phone_number_regex = RegexValidator(regex = r"^\+?1?\d{3}[-]\d{3}[-]\d{4}$") #allows 
+	phone_number = models.CharField(validators = [phone_number_regex], max_length = 15, unique = True)
+
+	class Role(models.TextChoices):
+		ADMIN = 'Admin', 'Can delete members'
+		REGULAR = 'Regular', "Can't delete members"
+	
+	role = models.CharField(
+        max_length=7,
+        choices=Role.choices,
+        default=Role.REGULAR,
+    )
+
+	def is_admin(self):
+		return self.role == self.Role.ADMIN
+            
