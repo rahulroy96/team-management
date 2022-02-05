93c523d5c39e5381382dc141ec462fe2e83beffe
Rahul Roy <rahulroy1996@gmail.com>
2022-02-05 02:56:50 -0500

    Changed to ModelForm instead of specifying the fields directly in view

--- members/views.py
+++ members/views.py
@@ -1,36 +1,30 @@
-from django.views.generic import (
-    ListView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, FormView
 from .models import Member
+from .forms import MemberForm
 
 
 class MemberListView(ListView):
-	model = Member
-	template_name = 'members/members_list.html'
-	context_object_name = 'members'
-	ordering = ['-f_name']
+    model = Member
+    template_name = "members/members_list.html"
+    context_object_name = "members"
+    ordering = ["-f_name"]
 
 
 class MemberCreateView(CreateView):
     model = Member
-    fields = ['f_name', 'l_name', 'email', 'phone_number', 'role']
-    template_name = 'members/member_add.html'
-    success_url = '/'
+    form_class = MemberForm
+    template_name = "members/member_add.html"
+    success_url = "/"
 
 
 class MemberUpdateView(UpdateView):
     model = Member
-    fields = ['f_name', 'l_name', 'email', 'phone_number', 'role']
-    template_name = 'members/member_edit.html'
-    success_url = '/'
+    form_class = MemberForm
+    template_name = "members/member_edit.html"
+    success_url = "/"
 
 
 class MemberDeleteView(DeleteView):
     model = Member
-    context_object_name = 'member'
-    success_url = '/'
-
-
+    context_object_name = "member"
+    success_url = "/"
