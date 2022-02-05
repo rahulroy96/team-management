e8f66c5e2e5a3ad84f7bff991dcd03341e850e8e
Rahul Roy <rahulroy1996@gmail.com>
2022-02-05 03:06:15 -0500

    added member to the route for creating member for consistancy

--- members/urls.py
+++ members/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from .views import (
-	MemberListView, 
-	MemberCreateView, 
-	MemberUpdateView, 
-	MemberDeleteView
-)
+from .views import MemberListView, MemberCreateView, MemberUpdateView, MemberDeleteView
 
 urlpatterns = [
-	path('', MemberListView.as_view(), name='members-list'),
-	path('add/', MemberCreateView.as_view(), name='member-add'),
-	path('member/<int:pk>/', MemberUpdateView.as_view(), name='member-edit'),
-	path('member/<int:pk>/delete', MemberDeleteView.as_view(), name='member-delete'),
+    path("", MemberListView.as_view(), name="members-list"),
+    path("member/add/", MemberCreateView.as_view(), name="member-add"),
+    path("member/<int:pk>/", MemberUpdateView.as_view(), name="member-edit"),
+    path("member/<int:pk>/delete", MemberDeleteView.as_view(), name="member-delete"),
 ]
