8d9f21cdd45502ddedc27227dff0170e85d86b55
Rahul Roy <rahulroy1996@gmail.com>
2022-02-05 19:09:29 -0500

    memberListView Test Cases

--- members/tests.py
+++ members/tests.py
@@ -31,3 +31,45 @@ class MemberModelTests(TestCase):
         self.assertIs(admin_member.is_admin(), False)
 
 
+def create_member(
+    f_name="test_f_name",
+    l_name="test_l_name",
+    email="test@test.com",
+    phone_number="3535351234",
+    role="regular",
+):
+    """
+    Creates a member with the given details
+    """
+    return Member.objects.create(
+        f_name=f_name, l_name=l_name, email=email, phone_number=phone_number, role=role
+    )
+
+
+class MemberListViewTests(TestCase):
+    def test_no_members(self):
+        response = self.client.get(reverse("members-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You have 0 team members.")
+        self.assertQuerysetEqual(response.context["members"], [])
+
+    def test_admin_member(self):
+        member = create_member(role="Admin")
+        response = self.client.get(reverse("members-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "admin")
+        self.assertQuerysetEqual(response.context["members"], [member])
+
+    def test_no_admin_member(self):
+        member = create_member(role="Regular")
+        response = self.client.get(reverse("members-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "admin")
+        self.assertQuerysetEqual(response.context["members"], [member])
+
+    def test_one_member(self):
+        member = create_member(role="Regular")
+        response = self.client.get(reverse("members-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You have 1 team member.")
+        self.assertQuerysetEqual(response.context["members"], [member])
