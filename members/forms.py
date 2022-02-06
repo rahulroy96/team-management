c12878006e8029d0f7fa4a233f9694f071641500
Rahul Roy <rahulroy1996@gmail.com>
2022-02-06 13:12:15 -0500

    Fixed bug in phone number cleaning code

--- members/forms.py
+++ members/forms.py
@@ -7,13 +7,15 @@ class MemberForm(ModelForm):
         model = Member
         fields = ["f_name", "l_name", "email", "phone_number", "role"]
         widgets = {
-            'role': RadioSelect(),
+            "role": RadioSelect(),
         }
 
     def clean_phone_number(self):
         # add '-' to the phone numbers if user didn't enter
         super().clean()
         phone_number = self.cleaned_data["phone_number"]
+        if len(phone_number) < 10:
+            return phone_number
         if phone_number[3] != "-":
             phone_number = phone_number[:3] + "-" + phone_number[3:]
 
