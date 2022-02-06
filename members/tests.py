1afb7e78a2451391d045c5291510536e8eca2a4d
Rahul Roy <rahulroy1996@gmail.com>
2022-02-05 19:07:09 -0500

    member model test cases for checking is_admin

--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Member
 
-# Create your tests here.
+# test cases which tests the MemberModel
+class MemberModelTests(TestCase):
+    def test_is_admin_with_admin_member(self):
+        """
+        is_admin returns true for members who are admins
+        """
+        admin_member = Member(
+            f_name="test_f_name",
+            l_name="test_l_name",
+            email="test@test.com",
+            phone_number="3234567890",
+            role="Admin",
+        )
+        self.assertIs(admin_member.is_admin(), True)
+
+    def test_is_admin_with_regular_member(self):
+        """
+        is_admin returns false for members who are not admin
+        """
+        admin_member = Member(
+            f_name="test_f_name",
+            l_name="test_l_name",
+            email="test@test.com",
+            phone_number="3234567890",
+            role="Regular",
+        )
+        self.assertIs(admin_member.is_admin(), False)
+
+
